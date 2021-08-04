38ca369623d80f41078e2b5f271a1a3800693e49
AP <ashis.n.patel@gmail.com>
2021-08-03 20:40:45 -0500

    Add gif generation in main content section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,59 @@ const contentEl = document.querySelector("#content");
 const gifBtnEl = document.querySelector("#gif-btn"); 
 
 const selectRandom = function(array,numItems) {
-    
+    let random = [];
+    for(let i=0; random.length < numItems; i++) {
+        let randomItem = array[Math.floor(Math.random()*array.length)]; 
+        if(!random.includes(randomItem)) {
+            random.push(randomItem);
+        }
+    }
+    return random; 
 }
 
 const displayGifs = function(gifArray) {
-    console.log(gifArray); 
+    // Delete old content in content section
+    contentEl.textContent = ""; 
+    // Create div to contain grid of 4 columns , thus '3 rows' for 12 items
+    const gridEl = document.createElement("div"); 
+    gridEl.classList = "grid grid-cols-4 justify-items-center gap-5";
+    // Populate grid of taken in array
+    for(let i=0; i < gifArray.length; i++) {
+        // Create div to hold each gif img element
+        const imgWrapperEl = document.createElement("div"); 
+        imgWrapperEl.classList = "col-span-1"; 
+        // Create image for each gif and add to img-wrapper
+        const imgEl = document.createElement("img"); 
+        imgEl.setAttribute("src",gifArray[i].images.original.url); 
+        imgEl.setAttribute("alt","");
+        imgEl.classList = "w-96 h-96"; 
+        imgWrapperEl.appendChild(imgEl);
+        // Add image-wrapper to grid
+        gridEl.appendChild(imgWrapperEl); 
+    }
+    // Add grid to content section
+    contentEl.appendChild(gridEl); 
 }
 
 const getGifs = function() {
     // Set potential tags that could be searched
     const potentialTags = ["kittens", "cats", "dogs", "puppies", "cute", "bees"];
-    // Select random tag from potential tags
-    const randomTag = potentialTags[Math.floor(Math.random()*potentialTags.length)]
+    // Select a random tag from potential tags
+    const randomTag = selectRandom(potentialTags,1); 
     console.log(randomTag); 
     const apiUrl = `https://api.giphy.com/v1/gifs/search?q=${randomTag}&api_key=8uu014p8mspMOtSIJu7z8PtcUxdwsM9x`;
 
     fetch(apiUrl).then(function (response) {
         if(response.ok) {
             response.json().then(function(data){
-                // Grab first 10 gifs from search, make this random? 
-                const numGifs = 10; 
+                // Grab 10 random gifs from the retrieved gif array 
+                const numGifs = 12; 
+                const retrievedGifs = data.data; 
+                const selectedGifs = selectRandom(retrievedGifs, numGifs); 
                 // Returned object is contains a "data" key that is an array of 50 gifs
                 // Grab numGifs from that array, maybe randomize
-                const selectedGifs = data.data.slice(0,numGifs);
                 displayGifs(selectedGifs);
+                console.log(selectedGifs); 
             })
         }
         else {

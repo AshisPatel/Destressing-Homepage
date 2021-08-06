c2dd9d2f9ec5b375e5091825b0692bf47c153f73
Anh Vuong <vta19797@gmail.com>
2021-08-06 17:24:53 -0500

    framework

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const jokeContentEL= document.getElementById('joke-content');
+const jokeSubmitbtnEl=document.getElementById('joke-submitbtn');
+const jokeRandombtnEl=document.getElementById('joke-randombtn');
+
+
+
+jokeRandombtnEl.addEventListener('click',generatejoke);
+generatejoke();
+async function generatejoke(){
+    //call Api
+    const jokefetch=await fetch('https://icanhazdadjoke.com/',{
+        headers:{
+            'Accept': 'application/json'
+        }
+    });
+
+    const jokeContent =await jokefetch.json();
+
+    console.log(jokeContent);
+    //Passing joke on screen
+    
+}
+
+
+
+

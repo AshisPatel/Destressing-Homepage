c4681bc522bfa631110467c04af42f33ac81ccc9
Anh Vuong <vta19797@gmail.com>
2021-08-06 19:31:06 -0500

    edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 const jokeContentEL= document.getElementById("joke-content");
 const jokeSubmitbtnEl=document.getElementById("joke-submitbtn");
 const jokeRandombtnEl=document.getElementById("joke-randombtn");
+const jokeSearchEl= document.getElementById("joke-search").value;
+
 
 
 
 jokeRandombtnEl.addEventListener('click',generatejoke);
-generatejoke();
-async function generatejoke(){
+
+var generatejoke = async function(){
     //call Api
     const jokefetch= await fetch('https://icanhazdadjoke.com/',{
         headers:{
@@ -19,6 +21,30 @@ async function generatejoke(){
     console.log(jokeContent.joke);
     //Passing joke on screen
     jokeContentEL.innerHTML=jokeContent.joke;  
+}
+generatejoke();
+
+//search bar 
+const jokeSearchBar= function(val){
+    val.preventDefault()
+    const searchvalue= val.target.value;
+    generatesearchjoke(searchvalue)
+        
+
+}
+const generatesearchjoke=async function (searchvalue){
+    const jokefetch= await fetch('https://icanhazdadjoke.com/search?term='+searchvalue,{
+        headers:{
+            'Accept': 'application/json'
+        }
+    });
+
+    const jokeContent =await jokefetch.json();
+
+    console.log(jokeContent.joke);
+    //Passing joke on screen
+    jokeContentEL.innerHTML=jokeContent.joke;  
+
 }
 
 

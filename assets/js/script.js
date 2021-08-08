fa13ae16c6ba803810b788c01bfa0ef5615bdc25
AP <ashis.n.patel@gmail.com>
2021-08-08 16:16:31 -0500

    Re-ordergetJoke to generate display after API fetch occurs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const quoteEl = document.querySelector("#quote-btn");
-const contentEl = document.querySelector("#content"); 
+const contentEl = document.querySelector("#content");
 const blobContainerEl = document.querySelector("#blobContainer");
 const logoBtnEl = document.querySelector("#logo-btn")
 
@@ -8,15 +8,15 @@ const gifBtnEl = document.querySelector("#gif-btn");
 const contentOptionsEl = document.querySelector('#content-options');
 
 const nextBtnEl = document.querySelector("#next-btn");
-const searchBtnEl = document.querySelector("#search-btn");  
-const searchModalEl = document.querySelector("#searchModal"); 
-const closeModalBtnEl = document.querySelector("#close-modal-btn"); 
+const searchBtnEl = document.querySelector("#search-btn");
+const searchModalEl = document.querySelector("#searchModal");
+const closeModalBtnEl = document.querySelector("#close-modal-btn");
 const modalChooseBtnEl = document.querySelector("#modal-choose-btn");
 const modalSearchBtnEl = document.querySelector("#modal-search-btn");
 const modalSearchFormEl = document.querySelector("#modal-search-form");
 const warningEl = document.querySelector(".warning-text");
 let prevGifTag = "";
-let currentContent = ""; 
+let currentContent = "";
 //  Utility functions
 
 const selectRandom = function (array, numItems) {
@@ -41,11 +41,11 @@ const getCurrentTime = function () {
 const getWelcomeMessage = function (hour) {
     // Initalize message variables
     let headerText = "";
-    let msgText = ""; 
+    let msgText = "";
 
     //Early-hours 
     if (hour >= 0 && hour < 6) {
-        console.log("Early-hours"); 
+        console.log("Early-hours");
         headerText = "Night Owl or Early Bird?";
         msgText = "Stay Awhile and Relax 😊";
     }
@@ -57,11 +57,11 @@ const getWelcomeMessage = function (hour) {
     }
     // Afternoon-hours
     if (hour >= 12 && hour < 18) {
-        console.log("Afternoon"); 
+        console.log("Afternoon");
         headerText = "Good Afternoon";
         msgText = "Stay Awhile and Relax 😊";
     }
-    
+
     // Evenning-hours
     if (hour >= 18 && hour < 24) {
         console.log("Evenning");
@@ -69,16 +69,16 @@ const getWelcomeMessage = function (hour) {
         msgText = "Stay Awhile and Relax 😊";
     }
 
-    displayWelcomeMessage(headerText,msgText); 
+    displayWelcomeMessage(headerText, msgText);
 }
 
-const displayWelcomeMessage = function(headerText,msgText) {
+const displayWelcomeMessage = function (headerText, msgText) {
     // Reset home page 
-    blobContainerEl.classList.remove("hide");  
-    blobContainerEl.classList.add("show"); 
+    blobContainerEl.classList.remove("hide");
+    blobContainerEl.classList.add("show");
 
-    nextBtnEl.classList.remove("show","my-10");
-    searchBtnEl.classList.remove("show","my-10");  
+    nextBtnEl.classList.remove("show", "my-10");
+    searchBtnEl.classList.remove("show", "my-10");
 
     contentEl.classList.remove("space-top-image");
     contentEl.textContent = "";
@@ -100,7 +100,7 @@ const logoBtnHandler = function (event) {
     // Grab time and message based on time
     getCurrentTime();
     // Reset selected menu to default option
-    contentOptionsEl.selectedIndex = 0; 
+    contentOptionsEl.selectedIndex = 0;
 }
 
 
@@ -112,30 +112,30 @@ const searchBtnHandler = function (event) {
 
     const searchCategoryEl = document.querySelector("#search-category");
     // Add current category to search header in modal 
-    searchCategoryEl.textContent = currentContent.charAt(0).toUpperCase() + currentContent.slice(1); 
+    searchCategoryEl.textContent = currentContent.charAt(0).toUpperCase() + currentContent.slice(1);
     searchModalEl.style.display = "block";
 }
 
-const modalChooseBtnHandler = function(event) {
-    
+const modalChooseBtnHandler = function (event) {
+
     // Hide modal 
     searchModalEl.style.display = "none";
     // Check which display is on the screen 
-    
+
     if (currentContent === "gif") {
-        getRandomGif(); 
+        getRandomGif();
     }
 
-    if (currentContent === "painting"){
+    if (currentContent === "painting") {
         // Insert function to call random painting
-        getArt(); 
+        getArt();
     }
 
-    if (currentContent === "quote"){
+    if (currentContent === "quote") {
         // Insert function to call random quote
     }
 
-    if (currentContent === "joke"){
+    if (currentContent === "joke") {
         // Insert function to call random joke 
         getjoke();
     }
@@ -143,7 +143,7 @@ const modalChooseBtnHandler = function(event) {
 
 const modalSearchHandler = function (event) {
     event.preventDefault();
-    
+
     const modalSearchInputEl = document.querySelector("#modal-search-input");
     const searchTag = modalSearchInputEl.value.trim();
 
@@ -162,29 +162,29 @@ const modalSearchHandler = function (event) {
     if (currentContent === "gif") {
         prevGifTag = searchTag;
         getGifs(searchTag);
-        
+
         modalSearchFormEl.reset();
     }
 
-    if (currentContent === "painting"){
+    if (currentContent === "painting") {
         // Insert function to call random painting
-        console.log("Painting Search Test"); 
+        console.log("Painting Search Test");
     }
 
-    if (currentContent === "quote"){
+    if (currentContent === "quote") {
         // Insert function to call random quote
     }
 
-    if (currentContent === "joke"){
+    if (currentContent === "joke") {
         // Insert function to call random joke
-        searchJoke(searchTag); 
+        searchJoke(searchTag);
         modalSearchFormEl.reset();
     }
 }
 
 // Functions related to gif generation 
 
-const getRandomGif = function(event) {
+const getRandomGif = function (event) {
     // Set potential tags that could be searched
     const potentialTags = ["kitten", "cat", "dog", "puppy", "cute", "wholesome"];
     // Select a random tag from potential tags
@@ -204,13 +204,12 @@ const getGifs = function (searchTag) {
                 // Grab a random gifs from the retrieved gif array 
                 const numGifs = 1;
                 const retrievedGifs = data.data;
-                console.log(retrievedGifs);
                 const selectedGifs = selectRandom(retrievedGifs, numGifs);
                 // Returned object is contains a "data" key that is an array of 50 gifs
                 // Grab numGifs from that array, maybe randomize
-               
+
                 displayGifs(selectedGifs);
-               
+
             })
         }
         else {
@@ -223,37 +222,37 @@ const displayGifs = function (gif) {
     currentContent = "gif";
     // Delete old content in content section
     blobContainerEl.classList.remove("show");
-    blobContainerEl.classList.add("hide");  
+    blobContainerEl.classList.add("hide");
     contentEl.textContent = "";
     contentEl.classList.add("space-top-image");
-  
-    const gifWrapper = document.createElement("div");
-    gifWrapper.setAttribute("style","width:800px;height:550px"); 
-    gifWrapper.classList = "flex justify-center";
-    const gifImg = document.createElement("img"); 
-    gifImg.classList.add("image-mask"); 
-    gifImg.setAttribute("style","width:480px;height:480px"); 
-    gifImg.setAttribute("src",gif[0].images.original.url); 
-    gifWrapper.appendChild(gifImg);
-    contentEl.appendChild(gifWrapper); 
 
-    nextBtnEl.classList.add("show","my-10");
+    const gifWrapper = document.createElement("div");
+    gifWrapper.setAttribute("style", "width:800px;height:550px");
+    gifWrapper.classList = "flex justify-center";
+    const gifImg = document.createElement("img");
+    gifImg.classList.add("image-mask");
+    gifImg.setAttribute("style", "width:480px;height:480px");
+    gifImg.setAttribute("src", gif[0].images.original.url);
+    gifWrapper.appendChild(gifImg);
+    contentEl.appendChild(gifWrapper);
+
+    nextBtnEl.classList.add("show", "my-10");
     searchBtnEl.classList.remove("hide");
-    searchBtnEl.classList.add("show","my-10");  
+    searchBtnEl.classList.add("show", "my-10");
 }
 
 
 // Functions regarding artwork generation 
-    
-const getArt = async function() {
+
+const getArt = async function () {
     // Hide the button while it's grabbing the response and show in the function
     nextBtnEl.classList.remove("show");
-    searchBtnEl.classList.remove("show");  
+    searchBtnEl.classList.remove("show");
 
     //To do: Need to add loader
 
     contentEl.innerHTML = '';
-    currentContent = "painting"; 
+    currentContent = "painting";
     blobContainerEl.classList.remove("show");
 
     // Get all objects that have an image and matches the query 'Painting'
@@ -264,9 +263,9 @@ const getArt = async function() {
 
     // Get IDs of the objects
     const museumArray = museumData.objectIDs;
-    
+
     // Get random ID from that array
-    const museumId = museumArray[Math.floor((Math.random()*museumArray.length))];
+    const museumId = museumArray[Math.floor((Math.random() * museumArray.length))];
 
     // Get object matching that ID
     const artResponse = await fetch(
@@ -279,7 +278,7 @@ const getArt = async function() {
     const artWrapper = document.createElement("div");
     const artImg = document.createElement("img");
     artImg.src = artSource;
-    artWrapper.setAttribute('style','width:800px;height:550px');
+    artWrapper.setAttribute('style', 'width:800px;height:550px');
 
     contentEl.appendChild(artWrapper);
     artWrapper.appendChild(artImg);
@@ -290,9 +289,9 @@ const getArt = async function() {
     nextBtnEl.classList.add("show", "my-10");
 
     nextBtnEl.textContent = `More artwork`;
-    
-    searchBtnEl.classList.remove("hide"); 
-    searchBtnEl.classList.add("show","my-10"); 
+
+    searchBtnEl.classList.remove("hide");
+    searchBtnEl.classList.add("show", "my-10");
 }
 
 // Function to handle displaying more of currently selected content 
@@ -318,158 +317,157 @@ const nextBtnHandler = function (event) {
     }
 }
 
-const closeModalBtnHandler = function(event) {
-      searchModalEl.style.display= "none"; 
+const closeModalBtnHandler = function (event) {
+    searchModalEl.style.display = "none";
 
 }
 
-const startQuotes = function(event){
-    
-    fetch("https://api.quotable.io/random").then(function(response){
-        
-        if(response.ok){
+const startQuotes = function (event) {
+
+    fetch("https://api.quotable.io/random").then(function (response) {
+
+        if (response.ok) {
             blobContainerEl.classList.remove("hide");
-            blobContainerEl.classList.add("show"); 
-            searchBtnEl.classList.remove("show","my-10");
-            searchBtnEl.classList.add("hide"); 
-            contentEl.textContent = ""; 
+            blobContainerEl.classList.add("show");
+            searchBtnEl.classList.remove("show", "my-10");
+            searchBtnEl.classList.add("hide");
+            contentEl.textContent = "";
             currentContent = "quote";
-            response.json().then(function(data){
-            //  console.log(data);
-            // alert("quotes Working!")
-            // console.log(data.content)
-            
-             const quoteCard = document.createElement("div");
-             quoteCard.classList = "w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800";
-             quoteCard.setAttribute("style","max-width: 500px")
+            response.json().then(function (data) {
+                //  console.log(data);
+                // alert("quotes Working!")
+                // console.log(data.content)
 
-             const quoteTextArea = document.createElement("div");
-             quoteTextArea.classList="w-full mb-10";
-             quoteCard.appendChild(quoteTextArea);
+                const quoteCard = document.createElement("div");
+                quoteCard.classList = "w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800";
+                quoteCard.setAttribute("style", "max-width: 500px")
 
-             const randomQuote= document.createElement("p")
-             randomQuote.classList="text-sm text-gray-600 text-center px-5"
-             randomQuote.textContent = '"'+ data.content+ '"';
-             quoteTextArea.appendChild(randomQuote);
-            
-             const autorArea =document.createElement("div");
-             autorArea.classList ="w-full";
-             quoteCard.appendChild(autorArea)
-             
-             const autorName = document.createElement("p");
-             autorName.classList ="text-md text-indigo-500 font-bold text-center";
-             autorName.textContent = data.author;
-             autorArea.appendChild(autorName);
+                const quoteTextArea = document.createElement("div");
+                quoteTextArea.classList = "w-full mb-10";
+                quoteCard.appendChild(quoteTextArea);
 
-             contentEl.appendChild(quoteCard);
+                const randomQuote = document.createElement("p")
+                randomQuote.classList = "text-sm text-gray-600 text-center px-5"
+                randomQuote.textContent = '"' + data.content + '"';
+                quoteTextArea.appendChild(randomQuote);
 
-             nextBtnEl.textContent="More quotes";
-             nextBtnEl.classList.add("show","my-10");
+                const autorArea = document.createElement("div");
+                autorArea.classList = "w-full";
+                quoteCard.appendChild(autorArea)
+
+                const autorName = document.createElement("p");
+                autorName.classList = "text-md text-indigo-500 font-bold text-center";
+                autorName.textContent = data.author;
+                autorArea.appendChild(autorName);
+
+                contentEl.appendChild(quoteCard);
+
+                nextBtnEl.textContent = "More quotes";
+                nextBtnEl.classList.add("show", "my-10");
             })
-        }else{
-        alert("link not working")
+        } else {
+            alert("link not working")
         }
     });
 };
 
 //Joke section
-const jokeContainer = document.createElement ("div");   
-const jokeContentEL = document.createElement ("div");
-jokeContainer.appendChild (jokeContentEL);
+// const jokeContainer = document.createElement ("div");   
+// const jokeContentEL = document.createElement ("div");
+// jokeContainer.appendChild (jokeContentEL);
 
-const getjoke = async function(){
-    
-    currentContent="joke";
-    
-    blobContainerEl.classList.remove("hide");  
-    blobContainerEl.classList.add("show"); 
-    nextBtnEl.classList.remove("show","my-10");
-    searchBtnEl.classList.remove("show","my-10");  
+const getjoke = async function () {
+
+    currentContent = "joke";
+
+
+    //call Api
+    const jokefetch = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    const jokeContent = await jokefetch.json();
+
+    //console.log(jokeContent.joke);
+    //Passing joke on screen
+    blobContainerEl.classList.remove("hide");
+    blobContainerEl.classList.add("show");
+    nextBtnEl.classList.remove("show", "my-10");
+    searchBtnEl.classList.remove("show", "my-10");
     contentEl.classList.remove("space-top-image");
     contentEl.textContent = "";
 
-    const jokeContainer = document.createElement ("div");
-    jokeContainer.classList= "jokeContainer w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800";
-    jokeContainer.setAttribute("style","max-width: 500px");
+    const jokeContainer = document.createElement("div");
+    jokeContainer.classList = "jokeContainer w-full mx-auto rounded-lg bg-white shadow-lg px-5 pt-5 pb-10 text-gray-800";
+    jokeContainer.setAttribute("style", "max-width: 500px");
 
-    
-    jokeContentEL.classList="w-full mb-10";
-    jokeContainer.appendChild (jokeContentEL);
+    const jokeContentEL = document.createElement("div");
+    jokeContentEL.classList = "w-full mb-10";
+    jokeContainer.appendChild(jokeContentEL);
     contentEl.appendChild(jokeContainer);
 
-    
-    nextBtnEl.textContent="More joke";
-    nextBtnEl.classList.add("show","my-10");
-    
+    jokeContentEL.innerHTML = jokeContent.joke;
 
-     //call Api
-     const jokefetch= await fetch('https://icanhazdadjoke.com/',{
-        headers:{
-            'Accept': 'application/json'
-        }
-    });
+    nextBtnEl.textContent = "More jokes";
+    nextBtnEl.classList.add("show", "my-10");
 
-    const jokeContent =await jokefetch.json();
+    searchBtnEl.classList.remove("hide");
+    searchBtnEl.classList.add("show", "my-10");
 
-    console.log(jokeContent.joke);
-    //Passing joke on screen
-    jokeContentEL.innerHTML=jokeContent.joke; 
-    
-    searchBtnEl.classList.remove("hide"); 
-    searchBtnEl.classList.add("show","my-10"); 
-   
 }
 //call for search
-async function searchJoke(searchTag){
-    
+async function searchJoke(searchTag) {
+
     //call Api
-    const jokefetch= await fetch('https://icanhazdadjoke.com/search?term=' +searchTag,{
-        headers:{
+    const jokefetch = await fetch('https://icanhazdadjoke.com/search?term=' + searchTag, {
+        headers: {
             'Accept': 'application/json'
         }
     });
 
-    const jokeContent =await jokefetch.json();
+    const jokeContent = await jokefetch.json();
 
-  
+
     //Passing joke on screen
-    jokeContentEL.innerHTML=jokeContent.joke;  
+    jokeContentEL.innerHTML = jokeContent.joke;
 }
 
- 
+
 
 
 
 
 // Run the function based on the value in the dropdown
-contentOptionsEl.addEventListener('change', function() {
+contentOptionsEl.addEventListener('change', function () {
     if (contentOptionsEl.value === 'painting') {
         getArt();
     }
-    
-    if(contentOptionsEl.value ==='quote'){
+
+    if (contentOptionsEl.value === 'quote') {
         startQuotes();
     }
 
     if (contentOptionsEl.value === 'gif') {
-        getRandomGif(); 
+        getRandomGif();
     }
 
     if (contentOptionsEl.value === 'joke') {
-       getjoke();
+        getjoke();
     }
 })
 
-searchBtnEl.addEventListener("click", searchBtnHandler); 
+searchBtnEl.addEventListener("click", searchBtnHandler);
 modalChooseBtnEl.addEventListener("click", modalChooseBtnHandler);
 modalSearchFormEl.addEventListener("submit", modalSearchHandler);
-nextBtnEl.addEventListener("click", nextBtnHandler);   
-closeModalBtnEl.addEventListener("click",closeModalBtnHandler); 
+nextBtnEl.addEventListener("click", nextBtnHandler);
+closeModalBtnEl.addEventListener("click", closeModalBtnHandler);
 
-logoBtnEl.addEventListener("click",logoBtnHandler);
+logoBtnEl.addEventListener("click", logoBtnHandler);
 
 // On load, generate welcome message
 
-window.onload = function() {
-    logoBtnHandler(); 
+window.onload = function () {
+    logoBtnHandler();
 }
